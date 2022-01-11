251c13a450b5795a35a35e1c0d32780b0618b8d4
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-01-11 15:03:22 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+const changeBgColor = node => {
+  node.style.backgroundColor = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  changeBgColor(document.body);
+  timerId = setInterval(() => {
+    changeBgColor(document.body);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
